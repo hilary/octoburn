d99341bb233c9ee0fc71bd59830b6262fb1e878a
Hilary Holz <hilary@hholz.com>
2012-09-07 20:06:07 -0700

    add TagIndexGenerator

--- plugins/tag_generator.rb
+++ plugins/tag_generator.rb
@@ -2,130 +2,118 @@
 #
 # Jekyll tag page generator.
 #
-# Version: 0.0.1 (20120906)
+# Version: 0.0.2 (20120907)
 #
 # Copyright (c) 2012 Hilary J Holz, http://hholz.com/
 # Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
 # 
-# After Dave Perrett's Category Generator plugin
-#
 # Creates tag pages for jekyll sites.
 #
-# _config.yml settings
-#   tag_dir                     : string: where to build the tag pages 
-#                                  default: 'tags'
-#   tag_title_prefix            : string: used before tag name in page title 
-#                                  default: 'posts tagged: '
-#   tag_meta_description_prefix : string: used before tag name in page description
-#                                  default: 'index of posts tagged: '
-#   tag_index_layout            : string: filename of layout for tag pages
-#                                  default: 'tag_index'
-#   tag_index_layout_extension  : string: extension of layout for tag pages
-#                                  default: 'html'
+# see GenerateTagIndexes class for _config.yml settings
+#
 module Jekyll
 
   class TagIndex < Page
 
-    def initialize(site, base, tag_dir, tag_index_layout, tag)
-      @site             = site
-      @base             = base
-      @dir              = tag_dir
-      @tag_index_layout = tag_index_layout
-      @name             = 'index.html'
-      self.process(@name)
-
-
-      tag_index_layout_extension = site.config['tag_index_layout_extension'] || 'html'
-      tag_index_layout_filename = "#{tag_index_layout}.#{tag_index_layout_extension}"
-
-      self.read_yaml(File.join(base, '_layouts'), tag_index_layout_filename)
-
-      self.data['tag'] = tag
-
-      title_prefix       = site.config['tag_title_prefix'] || 'Posts tagged: '
-      self.data['title'] = "#{title_prefix}#{tag}"
-
-      meta_description_prefix  = site.config['tag_meta_description_prefix'] 
-                                  || 'Index of posts tagged: '
-      self.data['description'] = "#{meta_description_prefix}#{tag}"
-    end
-
-  end
-
-  class TagAtomFeed < Page 
-
-    def initialize(site, base, tag_dir, tag)
+    def initialize(service, site, tag, name,
+                   source_dir, layout_dir, dir,
+                   layout_filename, title_prefix, description_prefix)
       @site = site
-      @base = base
-      @dir  = tag_dir
-      @name = 'atom.xml'
+      @base = source_dir
+      @dir  = dir
+      @name = name
       self.process(@name)
 
-      self.read_yaml(File.join(base, '_includes/custom'), 'tag_feed.xml')
+      self.read_yaml(layout_dir, layout_filename)
+
       self.data['tag'] = tag
-
-      title_prefix       = site.config['tag_title_prefix'] || 'posts tagged: '
       self.data['title'] = "#{title_prefix}#{tag}"
+      self.data['description'] = "#{description_prefix}#{tag}"
 
-      meta_description_prefix  = site.config['tag_meta_description_prefix']
-                                  || 'index of posts tagged: '
-      self.data['description'] = "#{meta_description_prefix}#{tag}"
-
-      self.data['feed_url'] = "#{tag_dir}/#{name}"
-    end
-
-  end
-
-  class Site # built-in Jekyll class with access to global site config information.
-
-    def write_tag_index(tag_dir, tag)
-      index = TagIndex.new(self, self.source, tag_dir, tag_index_layout, tag)
-      index.render(self.layouts, site_payload)
-      index.write(self.dest)
-      
-      self.pages << index # so Site::cleanup does not remove it.
-
-      if self.config['tag_feeds']
-        feed = TagAtomFeed.new(self, self.source, tag_dir, tag)
-        feed.render(self.layouts, site_payload)
-        feed.write(self.dest)
-
-        self.pages << feed # so Site::cleanup does not remove it.
-      end
-    end
-
-    def write_tag_indexes(tag_index_layout = 'tag_index')
-      if self.layouts.key? tag_index_layout
-        dir = self.config['tag_dir']
-        self.tags.keys.each do |tag|
-          tag_slug = tag.gsub(/_|\P{Word}/, '-').gsub(/-{2,}/, '-').downcase
-          if dir.nil? or dir.empty?
-            self.write_tag_index(tag_slug, tag)
-          else
-            self.write_tag_index(File.join(dir, tag_slug), tag)
-          end
-        end
-
-      else
-        throw "No #{tag_index_layout} layout found."
+      if service == :atom 
+        self.data['feed_url'] = "#{dir}/#{name}" 
       end
     end
 
   end
 
-  class GenerateTags < Generator
+  class GenerateTagIndexes < Generator
     safe true
     priority :low
 
-    def generate(site) # generate called by jekyll
-      if site.config['tag_index_layout']
-        site.write_tag_indexes(site.config['tag_index_layout'])
-      else
-        site.write_tag_indexes
+    TAG_DIR                    = 'tags'
+    TAG_TITLE_PREFIX           = ''
+    TAG_DESCRIPTION_PREFIX     = 'index of posts tagged: '
+    TAG_INDEX_LAYOUT           = 'tag_index'
+    TAG_INDEX_LAYOUT_EXTENSION = 'html'
+
+    TAG_FEEDS                  = false
+    TAG_FEED_DIR               = 'feeds'
+    TAG_FEED_LAYOUT            = 'tag_feed'
+
+    def generate(site)
+
+      source_dir            = site.source
+      layout_dir            = File.join(source_dir, '_layouts')
+      tag_dir               = site.config['tag_dir'] || TAG_DIR
+      index_layout_filename = tag_index_layout +
+        ".#{site.config['tag_index_layout_extension'] || TAG_INDEX_LAYOUT_EXTENSION}"
+      title_prefix          = site.config['tag_title_prefix'] || TAG_TITLE_PREFIX
+      description_prefix    = site.config['tag_description_prefix'] || TAG_DESCRIPTION_PREFIX
+
+      if site.config['tag_feeds']
+        feed_layout_filename = tag_feed_layout + '.xml'
+        feed_dir             = site.config['tag_feed_dir'] || TAG_FEED_DIR
       end
+
+      site.tags.keys.each do |tag|
+
+        index = TagIndex.new(:html, site, tag, "#{tag}_index.html",
+                             source_dir, layout_dir, tag_dir,
+                             index_layout_filename, title_prefix, description_prefix)
+
+        index.render(site.layouts, site_payload)
+        index.write(site.dest)
+        site.pages << index
+
+        if site.config['tag_feeds']
+
+          feed = TagIndex.new(:atom, site, tag, "#{tag}_feed.xml",
+                             source_dir, layout_dir, feed_dir,
+                             feed_layout_filename, title_prefix, description_prefix)
+
+          feed.render(site.layouts, site_payload)
+          feed.write(site.dest)
+          site.pages << feed
+        end
+
+      end # site.tags.keys.each
+
+    end # generate
+
+    private
+
+    def tag_index_layout
+
+      index_layout = site.config['tag_index_layout'] || TAG_INDEX_LAYOUT
+      unless site.layouts.key? index_layout
+        throw "Could not find layout #{index_layout}"
+      end
+
+      return index_layout
     end
 
-  end
+    def tag_feed_layout
+
+      feed_layout = site.config['tag_feed_layout'] || TAG_FEED_LAYOUT
+      unless site.layouts.key? feed_layout
+        throw "Could not find atom feed layout #{feed_layout}"
+      end
+        
+      return feed_layout
+    end
+
+  end # class GenerateTagIndexes
 
 end
 
